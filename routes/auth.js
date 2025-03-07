463e9e295109d3d552ae8ec71b8e9a26f29dca6a
Leepermatt <131700890+Leepermatt@users.noreply.github.com>
2025-03-07 11:40:23 -0800

    week8.4render update

--- routes/auth.js
+++ routes/auth.js
@@ -10,7 +10,7 @@ router.get("/github", passport.authenticate("github", { scope: ["user:email"] })
 router.get("/github/callback",
     passport.authenticate("github", { failureRedirect: "/" }),
     (req, res) => {
-        res.redirect("/dashboard");
+        res.redirect("/api-docs");
     }
 );
 
